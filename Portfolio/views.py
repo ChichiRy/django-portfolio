f7e5cd83109dfd99887ffa3db1a7a5c819e4e20a
unknown <yonechironga@gmail.com>
2023-10-26 11:26:02 +0300

    third commit

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -4,32 +4,32 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def about(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def contact(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def education(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def work_exp(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def honours(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def certifications(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
 
 
 def projects(request):
-    return render(request, 'template.html', '')
+    return render(request, 'template.html')
